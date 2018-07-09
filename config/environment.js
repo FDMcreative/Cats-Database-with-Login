c81472ea41a69e3c1ae3a2fb15e75f95527d9a18
FDMcreative <federico.delmonte@gmail.com>
2018-07-09 14:10:49 +0200

    fixed databse environment undefined

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,6 @@
-module.exports = {
-  port: process.env.PORT || 3000,
-  env: process.env.NODE_ENV || 'development',
-  dbURI: process.env.MONGODB_URI || `mongodb://localhost/cats-database-with-login-${this.env}`
-}
+const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || 'development';
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/cats-database-with-login-${env}`
+const sessionSecret = process.env.SESSION_SECRET || 'my awesome secret';
+
+module.exports = { port, env, dbURI, sessionSecret };
